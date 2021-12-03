cb093b141fc132c79465d39ffa2fe5231871d2a3
Thomas Bolon <tbolon@mb3m.fr>
2021-12-03 11:52:39 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿Day03_Part2();
+
+#pragma warning disable CS8321
+
+void Day03_Part2()
+{
+    // string[]
+    var lines = File.ReadAllText("Day03.txt")
+        .Split("\n", options: StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    // byte[lines.Length][12];
+    var values = lines
+        .Select(l => l.Select(c => c - 48).ToArray())
+        .ToArray();
+
+    // 12
+    var size = values[0].Length;
+
+    var i = Reduce(true);
+    var oxygen = Convert.ToInt32(lines[i], 2);
+
+    i = Reduce(false);
+    var co2 = Convert.ToInt32(lines[i], 2);
+
+    Console.WriteLine(oxygen * co2);
+
+    int Reduce(bool majority)
+    {
+        var eligibles = Enumerable.Range(0, values!.Length).ToList(); // all indexes
+
+        for (var i = 0; i < size; i++)
+        {
+            if (eligibles.Count <= 1)
+                break; // found it (or failed)
+
+            var count1 = eligibles.Sum(x => values[x][i]);
+            var count0 = eligibles.Count - count1;
+
+            if (count1 >= count0)
+                eligibles = eligibles.Where(e => values[e][i] == (majority ? 1 : 0)).ToList();
+            else if (count1 < count0)
+                eligibles = eligibles.Where(e => values[e][i] == (majority ? 0 : 1)).ToList();
+        }
+
+        System.Diagnostics.Debug.Assert(eligibles.Count == 1, eligibles.Count.ToString());
+
+        return eligibles.First();
+    }
+}
+
+void Day03()
+{
+    var lines = File.ReadAllText("Day03.txt")
+        .Split("\n", options: StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    var setBits = new int[12];
+
+    for (int i = 0; i < lines.Length; i++)
+    {
+        for (int j = 0; j < setBits.Length; j++)
+        {
+            setBits[j] += lines[i][j] == '1' ? 1 : 0;
+        }
+    }
+
+    var gammaRate = 0;
+    var epsilonRate = 0;
+    var median = lines.Length / 2; // 500
+    for (int i = 0; i < setBits.Length; i++)
+    {
+        if (setBits[i] > median)
+            gammaRate |= 1 << (setBits.Length - i - 1);
+        else
+            epsilonRate |= 1 << (setBits.Length - i - 1);
+    }
+
+    Console.WriteLine($"Gamma Rate = {gammaRate} ; Epsilon Rate = {epsilonRate} ; Answer = {gammaRate * epsilonRate}");
+}
+
+void Day02()
+{
+    var lines = File.ReadAllText("Day02.txt")
+        .Split("\n", options: StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    (int horizontal, int depth, int aim) position = (0, 0, 0);
+
+    for (int i = 0; i < lines.Length; i++)
+    {
+        var parts = lines[i].Split(' ').ToArray();
+        var verb = parts[0];
+        var offset = int.Parse(parts[1]);
+
+        position = verb switch
+        {
+            "forward" => (position.horizontal + offset, position.depth + (position.aim * offset), position.aim),
+            "down" => (position.horizontal, position.depth, position.aim + offset),
+            "up" => (position.horizontal, position.depth, position.aim - offset),
+            _ => position
+        };
+    }
+
+    Console.WriteLine(position.horizontal * position.depth);
+}
+
+void Day01()
+{
+    var lines = File.ReadAllText("Day01.txt")
+        .Split("\n", options: StringSplitOptions.RemoveEmptyEntries)
+        .Select(l => int.Parse(l.Trim()))
+        .ToArray();
+
+
+    var increasing = 0;
+    for (var i = 1; i < lines.Length; i++)
+    {
+        if (lines[i] > lines[i - 1]) increasing++;
+    }
+
+    Console.WriteLine(increasing);
+}
