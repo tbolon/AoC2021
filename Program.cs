297ffdc53b83477a96db63b04e445804c8d5ead6
Thomas Bolon <tbolon@mb3m.fr>
2021-12-14 14:22:04 +0100

    part2

--- Program.cs
+++ Program.cs
@@ -6,17 +6,86 @@ Day14_Part2();
 
 void Day14_Part2()
 {
-    var lines = Input.GetLines(14, sample: true).ToArray();
+    const int maxLevel = 40;
+    var lines = Input.GetLines(14, sample: false).ToArray();
     var polymerTemplate = lines[0].ToList();
-    var permutations = lines.Skip(1).Select(l => l.Split('-')).ToDictionary(a => a[0].Trim(), a => a[1][2]);
 
-    var score = new Dictionary<char, long>();
+    // on stocke les permutations dans un dictionaire uint => char ou la clé correspond au résultat de AsKey()
+    var permutations = lines.Skip(1).Select(l => l.Split('-')).ToDictionary(a => AsKey(a[0][0], a[0][1]), a => a[1][2]);
 
-    for (int i = 0; i < polymerTemplate.Count - 1; i++)
+    // on va optimiser la recherche récursive en stockant le tableau de score pour un ensemble paire + niveau de profondeur de récursivité
+    // sans ça, le calcul prend trop de temps
+    var shortcuts = new Dictionary<(uint, int), long[]>();
+
+    // le tableau de score est un tableau avec 0 = 'A'
+    // on l'initialise avec le nb de caractères du template
+    var finalScore = new long[26];
+    foreach (var c in polymerTemplate)
     {
-        var c = polymerTemplate[i];
-        var c2 = polymerTemplate[i + 1];
-        var pair = string.Concat(polymerTemplate[i], polymerTemplate[i + 1]);
+        finalScore[c - 'A']++;
+    }
+
+    // on va ensuite analyser chaque paire du template, et descendre en récursif x fois (= niveau)
+    // pour analyser chaque paire produite
+    for (int x = 1; x < polymerTemplate.Count; x++)
+    {
+        Visit(polymerTemplate[x - 1], polymerTemplate[x], 1, finalScore);
+    }
+
+    WriteLine(finalScore.Where(p => p != 0).Max() - finalScore.Where(p => p != 0).Min());
+    ReadKey();
+
+    // obtient une clé unique sur 32 bits à partir de 2 caractères de 16 bits
+    // utilisé pour essayer d'optimiser le lookup, pas sur que ce soit finalement utile
+    uint AsKey(char c1, char c2)
+    {
+        return ((uint)(c1 - 'A')) << 16 | ((uint)(c2 - 'A'));
+    }
+
+    // visite une paire XX avec le niveau de profondeur "level" et stocke le résultat dans le tableau de score donné
+    void Visit(char c1, char c2, int level, long[] parentScore)
+    {
+        if (level > maxLevel)
+        {
+            return;
+        }
+
+        var key = AsKey(c1, c2);
+
+        // si le résultat de cette combinaison paire + niveau a déjà été calculée on la réutilise
+        if (shortcuts.TryGetValue((key, level), out var shortcut))
+        {
+            Add(shortcut, parentScore);
+            return;
+        }
+
+        // nouveau score à calculer pour cette paire+profondeur
+        var score = new long[26];
+
+        var permutation = permutations[key];
+
+        // ajout du score de la permutation ajoutée
+        score[permutation - 'A']++;
+
+        // on ajoute le score de toutes les itérations successives pour la partie gauche
+        Visit(c1, permutation, level + 1, score);
+
+        // on ajoute le score de toutes les itérations successives pour la partie droite
+        Visit(permutation, c2, level + 1, score);
+
+        // on met en cache le score local
+        shortcuts[(key, level)] = score;
+
+        // on ajoute le score local au score parent
+        Add(score, parentScore);
+    }
+
+    void Add(long[] score, long[] parentScore)
+    {
+        for (int i = 0; i < parentScore.Length; i++)
+        {
+            parentScore[i] += score[i];
+        }
     }
 }
 
