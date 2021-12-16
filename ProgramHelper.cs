b9c934506549a430e2cec533218bec68da07f441
Thomas Bolon <tbolon@mb3m.fr>
2021-12-16 14:41:32 +0100

    refacto

--- ProgramHelper.cs
+++ ProgramHelper.cs
@@ -14,12 +14,19 @@
     public static void Assert(bool condition, string? message) => System.Diagnostics.Debug.Assert(condition, message);
 
     public static void ReadKey(bool intercept = true) => Console.ReadKey(intercept);
+
     public static void SetCursorPosition(int left, int top) => Console.SetCursorPosition(left, top);
+
     public static void Clear() => Console.Clear();
+
     public static void WriteLine(object value, ConsoleColor? color = null) => WriteLine(value?.ToString(), color);
+
     public static void WriteLine(string? message, ConsoleColor? color = null) => Console.Write(message + Environment.NewLine, color);
+
     public static void WriteLine() => Console.Write(Environment.NewLine);
+
     public static void Write(object value, ConsoleColor? color = null) => Write(value?.ToString(), color);
+
     public static void Write(string? message, ConsoleColor? color = null)
     {
         ConsoleColor? previousColor = null;
